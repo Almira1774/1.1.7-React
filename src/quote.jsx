d013a8997c838aa7cc8c1f7eb4b559299f668616
Almira1774 <bezzbashennaya@gmail.com>
2026-02-24 12:54:28 +0300

    Initial commit

---
+++ src/quote.jsx
@@ -0,0 +1,9 @@
+import {createElement} from 'react'
+import './quote.css'
+
+const Quote = ()=>{
+  return createElement('h1', {classname: 'title'},  '«Заблудился в лесу — иди домой»  ©Джейсон Стетхем')
+
+} 
+
+export default Quote
